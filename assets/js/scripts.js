2f8cf369f67c36ae172b10773815e1132f3db0ca
Isaac Kleimmann Graper <isaac.graper@orbenk.br>
2024-06-24 13:26:57 -0300

    Add hover effect

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,27 +1,3 @@
-
-// scroll arrow function
-
-window.addEventListener('scroll', function() {
-    const content = document.querySelector('.content');
-    const contentTop = content.getBoundingClientRect().top;
-    const windowHeight = window.innerHeight;
-
-    if (contentTop < windowHeight / 1.5) {
-        content.style.opacity = '1';
-    }
-});
-
-window.addEventListener('scroll', function() {
-    const boxes = document.querySelectorAll('.box');
-
-    boxes.forEach(box => {
-        const boxTop = box.getBoundingClientRect().top;
-        const windowHeight = window.innerHeight;
-
-        if (boxTop < windowHeight * 0.75) {
-            box.classList.add('in-view');
-        } else {
-            box.classList.remove('in-view');
-        }
-    });
+document.getElementById('act-arrow').addEventListener('click', function() {
+    document.getElementById('projects-section').scrollIntoView({ behavior: 'smooth' });
 });
