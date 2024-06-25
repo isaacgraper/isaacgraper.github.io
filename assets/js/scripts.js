6a2718b6eddd3c61854bb5af01211377fe44ff47
Isaac Kleimmann Graper <isaac.graper@orbenk.br>
2024-06-25 16:10:30 -0300

    Update function with correction

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,21 +1,30 @@
-function scrollSuave(links, options = { behavior: 'smooth', block: 'start' }) {
+/**
+ * Função para adicionar comportamento de rolagem suave aos links internos.
+ * @param {string} links - Seletor CSS dos links internos.
+ * @param {object} options - Opções de rolagem suave (opcional). Padrão: { behavior: 'smooth', block: 'start' }.
+ * @returns {undefined}
+ */
+function scrollSmooth(links, options = { behavior: 'smooth', block: 'start' }) {
     function scrollToSection(e) {
       e.preventDefault();
-  
       const href = e.currentTarget.getAttribute('href');
       const section = document.querySelector(href);
-  
       if (section) {
         section.scrollIntoView(options);
       } else {
-        console.warn(`Element with href "${href}" not found!`);
-        // Optionally, provide user feedback (e.g., alert or visual cue)
+        console.error(`Element with href "${href}" not found!`);
       }
     }
-  
+
     if (links.length > 0) {
-      links.forEach(link => link.addEventListener('click', scrollToSection));
-    } else {
-      links.addEventListener('click', scrollToSection);
-    }
+        links.forEach(link => link.addEventListener('click', scrollToSection));
+   } else {
+        links.addEventListener('click', scrollToSection);
+   }
   }
+  
+  const links = document.querySelectorAll('.nav-item a');
+  scrollSmooth(links);
+
+  const arrow = document.querySelector('#act-arrow');
+  scrollSmooth(arrow);
