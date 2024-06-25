a36c5f0f6800a249919bccdf1b47473f20d1b6ff
Isaac Kleimmann Graper <isaac.graper@orbenk.br>
2024-06-25 15:50:47 -0300

    Add smooth scroll function

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,29 @@
-document.getElementById('act-arrow').addEventListener('click', function() {
-    document.getElementById('projects-section').scrollIntoView({ behavior: 'smooth' });
-});
+/**
+ * Função para adicionar comportamento de rolagem suave aos links internos.
+ * @param {string} links - Seletor CSS dos links internos.
+ * @param {object} options - Opções de rolagem suave (opcional). Padrão: { behavior: 'smooth', block: 'start' }.
+ * @returns {undefined}
+ */
+function scrollSuave(links, options = { behavior: 'smooth', block: 'start' }) {
+    function scrollToSection(e) {
+      e.preventDefault();
+      const href = e.currentTarget.getAttribute('href');
+      const section = document.querySelector(href);
+      if (section) {
+        section.scrollIntoView(options);
+      } else {
+        console.error(`Element with href "${href}" not found!`);
+      }
+    }
+
+    if (links.length > 0) {
+        links.forEach(link => link.addEventListener('click', scrollToSection));
+   } else {
+        links.addEventListener('click', scrollToSection);
+   }
+  }
+  
+  const links = document.querySelectorAll('.navlinks a');
+  scrollSuave(links);
+  const arrow = document.querySelector('#act-arrow');
+  scrollSuave(arrow);
