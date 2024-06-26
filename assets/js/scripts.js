9c6d1d9e952ba846feaf17f26f3d0514643d2a20
Isaac Kleimmann Graper <isaac.graper@orbenk.br>
2024-06-26 08:37:09 -0300

    Add arrow animation

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,42 +1,41 @@
-function scrollSmooth(links, options = { behavior: 'smooth', block: 'start' }) {
-    function scrollToSection(e) {
-      e.preventDefault();
-      const href = e.currentTarget.getAttribute('href');
-      const section = document.querySelector(href);
-      if (section) {
-        section.scrollIntoView(options);
-      } else {
-        console.error(`Element with href "${href}" not found!`);
-      }
+function scrollSmooth(links, options = { behavior: "smooth", block: "start" }) {
+  function scrollToSection(e) {
+    e.preventDefault();
+    const href = e.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    if (section) {
+      section.scrollIntoView(options);
+    } else {
+      console.error(`Element with href "${href}" not found!`);
     }
-
-    if (links.length > 0) {
-        links.forEach(link => link.addEventListener('click', scrollToSection));
-   } else {
-        links.addEventListener('click', scrollToSection);
-   }
   }
-  
-  const links = document.querySelectorAll('.nav-item a');
-  scrollSmooth(links);
 
-  const arrow = document.querySelector('#act-arrow');
-  scrollSmooth(arrow);
+  if (links.length > 0) {
+    links.forEach((link) => link.addEventListener("click", scrollToSection));
+  } else {
+    links.addEventListener("click", scrollToSection);
+  }
+}
 
-  anime({
-    targets: '.content',
-    translateX: [-80, 0],
-    easing: 'easeInOutQuad',
-    opacity: [0, 1]
-  });
+const links = document.querySelectorAll(".nav-item a");
+scrollSmooth(links);
 
-  anime({
-    targets: '.line-right',
-    rotate: '-28deg',
-  });
+const arrow = document.querySelector("#act-arrow");
+scrollSmooth(arrow);
 
-  anime({
-    targets: '.line-left',
-    rotate: '28deg',
-    
-  });
+anime({
+  targets: ".content",
+  translateX: [-80, 0],
+  easing: "easeInOutQuad",
+  opacity: [0, 1],
+});
+
+anime({
+  targets: ".line-right",
+  rotate: "-28deg",
+});
+
+anime({
+  targets: ".line-left",
+  rotate: "28deg",
+});
