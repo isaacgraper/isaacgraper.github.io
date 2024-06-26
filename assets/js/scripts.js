e5de019ef000375f5ec6bbb06c4cf70f6dcdcf05
Isaac Graper <isaac.graper@gmail.com>
2024-06-26 08:40:33 -0300

    Moved anime functions

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -22,21 +22,3 @@ scrollSmooth(links);
 
 const arrow = document.querySelector("#act-arrow");
 scrollSmooth(arrow);
-
-anime({
-  targets: ".content",
-  translateX: [-80, 0],
-  easing: "easeInOutQuad",
-  opacity: [0, 1],
-});
-
-  anime({
-    targets: '.line-right',
-    rotate: '-28deg',
-  });
-
-  anime({
-    targets: '.line-left',
-    rotate: '28deg',
-    
-  });
